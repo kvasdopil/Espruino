f8f2bbb3a7fc513d1ffcc48c3241bd78f74e0489
Alexey Guskov <lexag@mail.ru>
2020-04-15 23:19:58 +0200

    code cleanup

--- libs/spim/jswrap_spim.c
+++ libs/spim/jswrap_spim.c
@@ -21,6 +21,8 @@ static const nrfx_spim_t spi = NRFX_SPIM_INSTANCE(SPI_INSTANCE);
 
 static volatile bool spi_xfer_done;  /**< Flag used to indicate that SPI instance completed the transfer. */
 
+bool useMiso = false;
+
 void spim_event_handler(nrfx_spim_evt_t const * event, void * context)
 {
   spi_xfer_done = true;
@@ -45,12 +47,12 @@ JsVar *jswrap_spim_setup(JsVar *options) {
   }
 
   nrfx_spim_config_t spi_config = NRFX_SPIM_DEFAULT_CONFIG;
-  int miso = 0xff;
-  int mosi = 0xff;
-  int sck = 0xff;
-  int dcx = 0xff;
-  int ss = 0xff;
-  bool use_hw_ss = true;
+  int miso            = NRFX_SPIM_PIN_NOT_USED;
+  int mosi            = NRFX_SPIM_PIN_NOT_USED;
+  int sck             = NRFX_SPIM_PIN_NOT_USED;
+  int dcx             = NRFX_SPIM_PIN_NOT_USED;
+  int ss              = NRFX_SPIM_PIN_NOT_USED;
+  bool use_hw_ss      = true;
   bool ss_active_high = false;
   JsVar *speed = 0;
 
@@ -92,6 +94,7 @@ JsVar *jswrap_spim_setup(JsVar *options) {
       spi_config.frequency = NRF_SPIM_FREQ_1M;
     }
   }
+  useMiso = miso != 0xff;
 
   int result = nrfx_spim_init(&spi, &spi_config, spim_event_handler, NULL);
 
@@ -125,7 +128,7 @@ JsVar *jswrap_spim_setup(JsVar *options) {
   "name" : "send",
   "generate" : "jswrap_spim_send",
   "params" : [
-    ["options","JsVar","Send data to SPIM interface"],
+    ["data","JsVar","Data to send to SPIM interface"],
     ["cmdBytes","int32","Number of command bytes in the buffer"]
   ],
   "return" : ["JsVar","nothing"]
@@ -133,14 +136,19 @@ JsVar *jswrap_spim_setup(JsVar *options) {
 Send data via SPIM interface
 */
 JsVar *jswrap_spim_send(JsVar *buffer, int cmdBytes) {
-  JSV_GET_AS_CHAR_ARRAY(data, dataLen, buffer);
+  unsigned char *rx_data = NULL;
 
-  unsigned char *buf = (unsigned char *)alloca((size_t)dataLen + 1);
-  memset(buf, 0, dataLen + 1);
+  JSV_GET_AS_CHAR_ARRAY(tx_data, tx_size, buffer);
 
-  nrfx_spim_xfer_desc_t xfer_desc = NRFX_SPIM_XFER_TRX(data, dataLen, buf, dataLen + 1);
+  size_t rx_size = useMiso ? tx_size : 0;
+  if (rx_size) {
+    rx_data = (unsigned char *)alloca(rx_size);
+    memset(rx_data, 0, tx_size);
+  } 
 
   spi_xfer_done = false;
+
+  nrfx_spim_xfer_desc_t xfer_desc = NRFX_SPIM_SINGLE_XFER(tx_data, tx_size, rx_data, rx_size);
 
   int result = nrfx_spim_xfer_dcx(&spi, &xfer_desc, 0, cmdBytes);
   if (result != NRFX_SUCCESS) {
@@ -153,16 +161,21 @@ JsVar *jswrap_spim_send(JsVar *buffer, int cmdBytes) {
     __WFE();
   }
 
-  JsVar *array = jsvNewTypedArray(ARRAYBUFFERVIEW_UINT8, dataLen);
-  if (array) {
+  if (rx_size) {
+    JsVar *array = jsvNewTypedArray(ARRAYBUFFERVIEW_UINT8, rx_size);
+    if (!array) {
+      jsExceptionHere(JSET_ERROR, "Cannot allocate array");
+    }
     JsvArrayBufferIterator it;
     jsvArrayBufferIteratorNew(&it, array, 0);
     unsigned int i;
-    for (i=0; i<(unsigned)dataLen; i++) {
-      jsvArrayBufferIteratorSetByteValue(&it, (char)buf[i]);
+    for (i=0; i<(unsigned)rx_size; i++) {
+      jsvArrayBufferIteratorSetByteValue(&it, (char)rx_data[i]);
       jsvArrayBufferIteratorNext(&it);
     }
     jsvArrayBufferIteratorFree(&it);
+    return array;
   }
-  return array;
+  
+  return 0;
 }
