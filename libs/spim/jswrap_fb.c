04e7bd4623b8ce2f9f27e1cdf873237c792573b9
Alexey Guskov <lexag@mail.ru>
2020-04-25 18:04:54 +0200

    rects api

--- libs/spim/jswrap_fb.c
+++ libs/spim/jswrap_fb.c
@@ -22,63 +22,6 @@ This library provides functions to work with in-memory framebuffer
 #define FB_BPP sizeof(uint16_t)
 uint16_t fb[FB_WIDTH * FB_HEIGHT];
 
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "flip",
-  "generate" : "jswrap_fb_flip",
-  "params" : [],
-  "return" : ["JsVar","nothing"]
-}
-Send framebuffer to SPIM3 interface
-*/
-JsVar *jswrap_fb_flip() {
-  int half_length = FB_WIDTH * 120;
-  uint8_t b1[] = {0x2A, 0, 0, FB_HEIGHT >> 8, FB_HEIGHT && 0xff};
-  uint8_t b2[] = {0x2B, 0, 0, FB_WIDTH >> 8, FB_WIDTH && 0xff};
-  uint8_t b3[] = {0x2C};
-
-  // FIXME: add error check here
-  // spim_send_sync(b1, 5, 1);
-  // spim_send_sync(b2, 5, 1);
-  spim_send_sync(b3, 1, 1);
-
-  int result = spim_send_sync(fb, half_length * FB_BPP, 0);
-  if (result) {
-    jsExceptionHere(JSET_ERROR, "Cannot send data: error %d", result);
-    return 0;
-  }
-  result = spim_send_sync(fb + half_length, half_length * FB_BPP, 0);
-  if (result) {
-    jsExceptionHere(JSET_ERROR, "Cannot send data: error %d", result);
-    return 0;
-  }
-
-  return 0;
-}
-
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "clear",
-  "generate" : "jswrap_fb_clear",
-  "params" : [
-  ],
-  "return" : ["JsVar","nothing"]
-}
-Fill framebuffer with zeroes
-*/
-JsVar *jswrap_fb_clear() {
-  uint16_t* end = fb + FB_WIDTH * FB_HEIGHT;
-  uint16_t* ptr = fb;
-  while(ptr != end) {
-    *ptr = 0;
-    ptr++;
-  }
-
-  return 0;
-}
-
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
       __typeof__ (b) _b = (b); \
@@ -134,29 +77,15 @@ JsVar *jswrap_fb_blit(JsVar* buffer, int w, int h) {
     return 0;
   }
 
-  // const ymin = max(0, blit_y);
-  // const ymax = min(blit_y + h, FB_HEIGHT);
-  // const xmin = max(0, blit_x);
-  // const xmax = min(blit_x + w, FB_WIDTH);
+  for(int y = 0; y < h; y++) {
+    for(int x = 0; x < w; x++) {
+      int tx = blit_x + x;
+      int ty = blit_y + y;
+      if (tx < 0 || ty < 0 || tx >= FB_WIDTH || ty >= FB_HEIGHT) {
+        continue;
+      }
 
-  // TODO: optimize
-  // for(int sy = ymin; sy < ymax; sy++) {
-  //   int offy = sy - blit_y;
-  //   for(int sx = xmin; sy < xmax; sx++) {
-  //     int offx = sx - blit_x;
-  //     uint16_t val = buf16[offx + offy * w];
-  //     fb[sx + sy * FB_WIDTH] = val;
-  //   }
-  // }
-  // for(int yy = 0; yy<h; yy++) {
-  //   for(int xx = 0; xx<w; xx++) {
-  //     fb[blit_x + xx + (blit_y + yy) * FB_WIDTH] = buf16[xx + yy * w];
-  //   }
-  // }
-
-  for(int yy = 0;yy<h; yy++) {
-    for(int xx=0; xx<w; xx++) {
-      jswrap_fb_set(blit_x + xx, blit_y + yy, buf16[xx + yy * w]);
+      fb[tx + ty*FB_WIDTH] = buf16[x + y * w];
     }
   }
 
@@ -166,43 +95,149 @@ JsVar *jswrap_fb_blit(JsVar* buffer, int w, int h) {
 /*JSON{
   "type" : "staticmethod",
   "class" : "fb",
-  "name" : "setColor",
-  "generate" : "jswrap_fb_set_color",
+  "name" : "color",
+  "generate" : "jswrap_fb_color",
   "params" : [
     ["r","int","Red"],
     ["g","int","Green"],
     ["b","int","Blue"]
   ],
-  "return" : ["JsVar","nothing"]
+  "return" : ["int","color"]
 }
-Draw a filled rect on a buffer
+Convert color from [r,g,b] to int value
 */
 static uint16_t current_color = 0;
-JsVar* jswrap_fb_set_color(int r, int g, int b) {
-  current_color = (r >> 3 << 11) + (g >> 2 << 5) + (b >> 3);
-  uint8_t* bytes = &current_color;
+JsVar* jswrap_fb_color(int r, int g, int b) {
+  uint16_t color = (r >> 3 << 11) + (g >> 2 << 5) + (b >> 3);
+  uint8_t* bytes = &color;
   uint8_t bb = bytes[0];
   bytes[0] = bytes[1];
   bytes[1] = bb;
+  return color;
+}
+
+typedef struct {
+  int x;
+  int y;
+  int w;
+  int h;
+  int c;
+  void* next;
+  int id;
+} fb_rect;
+
+fb_rect* root = NULL;
+int next_id = 1;
+
+/*JSON{
+  "type" : "staticmethod",
+  "class" : "fb",
+  "name" : "init",
+  "generate" : "jswrap_fb_init",
+  "params" : [
+  ],
+  "return" : ["int","nothingd"]
+}
+Create a rect resource
+*/
+int jswrap_fb_init() {
+  root = NULL;
+  next_id = 1;
   return 0;
 }
 
 /*JSON{
   "type" : "staticmethod",
   "class" : "fb",
-  "name" : "fill",
-  "generate" : "jswrap_fb_fill",
+  "name" : "createRect",
+  "generate" : "jswrap_fb_create_rect",
   "params" : [
-    ["x","int","Start of fill"],
-    ["y","int","Start of fill"],
-    ["w","int","Width of fill"],
-    ["h","int","Height of fill"]
+    ["opt","JsVar","rect params"]
   ],
-  "return" : ["JsVar","nothing"]
+  "return" : ["int","rect id"]
 }
-Draw a filled rect on a buffer
+Create a rect resource
 */
-JsVar* jswrap_fb_fill(int x, int y, int w, int h) {
+int jswrap_fb_create_rect(JsVar* opt) {
+  fb_rect* rect = calloc(sizeof(fb_rect), 1);
+
+  rect->id = next_id++;
+  rect->x = 0;
+  rect->y = 0;
+  rect->w = 0;
+  rect->h = 0;
+  rect->c = 0;
+  rect->next = root;
+  root = rect;
+
+  jsvConfigObject configs[] = {
+      {"x", JSV_INTEGER, &(rect->x)},
+      {"y", JSV_INTEGER, &(rect->y)},
+      {"w", JSV_INTEGER, &(rect->w)},
+      {"h", JSV_INTEGER, &(rect->h)},
+      {"c", JSV_INTEGER, &(rect->c)},
+  };
+  
+  if (!jsvReadConfigObject(opt, configs, sizeof(configs) / sizeof(jsvConfigObject))) {
+    jsExceptionHere(JSET_ERROR, "Invalid arguments for rect");
+    return 0;
+  }
+
+  return rect->id;
+}
+
+/*JSON{
+  "type" : "staticmethod",
+  "class" : "fb",
+  "name" : "updateRect",
+  "generate" : "jswrap_fb_update_rect",
+  "params" : [
+    ["id","int","rect id"],
+    ["opt","JsVar","rect params"]
+  ],
+  "return" : ["int","nothing"]
+}
+Update a rect resource
+*/
+int jswrap_fb_update_rect(int id, JsVar* opt) {
+  fb_rect* rect = root;
+
+  // find rect
+  while(rect && rect->id != id) {
+    rect = rect->next;
+  }
+
+  if (!rect) {
+    jsExceptionHere(JSET_ERROR, "Resource not found");
+    return 0;
+  }
+
+  jsvConfigObject configs[] = {
+      {"x", JSV_INTEGER, &(rect->x)},
+      {"y", JSV_INTEGER, &(rect->y)},
+      {"w", JSV_INTEGER, &(rect->w)},
+      {"h", JSV_INTEGER, &(rect->h)},
+      {"c", JSV_INTEGER, &(rect->c)},
+  };
+  
+  if (!jsvReadConfigObject(opt, configs, sizeof(configs) / sizeof(jsvConfigObject))) {
+    jsExceptionHere(JSET_ERROR, "Invalid arguments for rect");
+    return 0;
+  }
+
+  return 0;
+}
+
+void fb_clear(int color) {
+  uint16_t* end = fb + FB_WIDTH * FB_HEIGHT;
+  uint16_t* ptr = fb;
+  while(ptr != end) {
+    *ptr = color;
+    ptr++;
+  }
+}
+
+void fb_fill(int x, int y, int w, int h) {
   // TODO: optimize
   const ymin = max(0, y);
   const ymax = min(y + h, FB_HEIGHT);
@@ -214,134 +249,55 @@ JsVar* jswrap_fb_fill(int x, int y, int w, int h) {
       fb[xx + yy * FB_WIDTH] = current_color;
     }
   }
-  return 0;
 }
 
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "get",
-  "generate" : "jswrap_fb_get",
-  "params" : [
-    ["x","int","Width of image"],
-    ["y","int","Height of image"]
-  ],
-  "return" : ["int","pixel value"]
-}
-Get a pixel value of fb
-*/
-int jswrap_fb_get(int x, int y) {
-  if (y < 0 || x < 0 || x >= FB_WIDTH || y >= FB_HEIGHT) {
-    return 0;
+void fb_flip() {
+  int half_length = FB_WIDTH * 120;
+  uint8_t b1[] = {0x2A, 0, 0, FB_HEIGHT >> 8, FB_HEIGHT && 0xff};
+  uint8_t b2[] = {0x2B, 0, 0, FB_WIDTH >> 8, FB_WIDTH && 0xff};
+  uint8_t b3[] = {0x2C};
+
+  // FIXME: add error check here
+  // spim_send_sync(b1, 5, 1);
+  // spim_send_sync(b2, 5, 1);
+  spim_send_sync(b3, 1, 1);
+
+  int result = spim_send_sync(fb, half_length * FB_BPP, 0);
+  if (result) {
+    jsExceptionHere(JSET_ERROR, "Cannot send data: error %d", result);
+    return;
   }
-
-  return fb[x + y * FB_WIDTH];
-}
-
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "set",
-  "generate" : "jswrap_fb_set",
-  "params" : [
-    ["x","int","Width of image"],
-    ["y","int","Height of image"],
-    ["c","int","Value of the pixel"]
-  ],
-  "return" : ["JsVar","nothing"]
-}
-Get a pixel value of fb
-*/
-JsVar* jswrap_fb_set(int x, int y, int c) {
-  if (y < 0 || x < 0 || x >= FB_WIDTH || y >= FB_HEIGHT) {
-    return 0;
+  result = spim_send_sync(fb + half_length, half_length * FB_BPP, 0);
+  if (result) {
+    jsExceptionHere(JSET_ERROR, "Cannot send data: error %d", result);
+    return;
   }
-
-  fb[x + y*FB_WIDTH] = c;
-
-  return 0;
-}
-
-typedef struct {
-  int x;
-  int y;
-  int w;
-  int h;
-  int c;
-} fb_rect;
-
-fb_rect rect0;
-
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "createRect",
-  "generate" : "jswrap_fb_create_rect",
-  "params" : [
-    ["opt","JsVar","rect params"]
-  ],
-  "return" : ["int","nothing"]
-}
-Create a rect resource
-*/
-int jswrap_fb_create_rect(JsVar* opt) {
-  jsvConfigObject configs[] = {
-      {"x", JSV_INTEGER, &rect0.x},
-      {"y", JSV_INTEGER, &rect0.y},
-      {"w", JSV_INTEGER, &rect0.w},
-      {"h", JSV_INTEGER, &rect0.h},
-      {"c", JSV_INTEGER, &rect0.c},
-  };
-  
-  if (!jsvReadConfigObject(opt, configs, sizeof(configs) / sizeof(jsvConfigObject))) {
-    jsExceptionHere(JSET_ERROR, "Invalid arguments for rect");
-    return 0;
-  }
-
-  return 0;
 }
 
 /*JSON{
   "type" : "staticmethod",
   "class" : "fb",
-  "name" : "updateRect",
-  "generate" : "jswrap_fb_update_rect",
-  "params" : [
-    ["opt","JsVar","rect params"]
-  ],
+  "name" : "render",
+  "generate" : "jswrap_fb_render",
   "return" : ["int","nothing"]
 }
 Update a rect resource
 */
-int jswrap_fb_update_rect(JsVar* opt) {
-  jsvConfigObject configs[] = {
-      {"x", JSV_INTEGER, &rect0.x},
-      {"y", JSV_INTEGER, &rect0.y},
-      {"w", JSV_INTEGER, &rect0.w},
-      {"h", JSV_INTEGER, &rect0.h},
-      {"c", JSV_INTEGER, &rect0.c},
-  };
-  
-  if (!jsvReadConfigObject(opt, configs, sizeof(configs) / sizeof(jsvConfigObject))) {
-    jsExceptionHere(JSET_ERROR, "Invalid arguments for rect");
-    return 0;
+int jswrap_fb_render(int id) {
+  // clear framebuffer
+  fb_clear(0);
+
+  // render all rects
+  fb_rect* rect = root;
+  while(rect) {
+    current_color = rect->c;
+    fb_fill(rect->x, rect->y, rect->w, rect->h);
+
+    rect = rect->next;
   }
 
+  // send data to display
+  fb_flip();
   return 0;
 }
 
-/*JSON{
-  "type" : "staticmethod",
-  "class" : "fb",
-  "name" : "renderRect",
-  "generate" : "jswrap_fb_render_rect",
-  "params" : [
-  ],
-  "return" : ["int","nothing"]
-}
-Update a rect resource
-*/
-int jswrap_fb_render_rect() {
-  current_color = rect0.c;
-  jswrap_fb_fill(rect0.x, rect0.y, rect0.w, rect0.h);
-}
--- libs/spim/jswrap_fb.h
+++ libs/spim/jswrap_fb.h
@@ -1,12 +1,9 @@
-JsVar *jswrap_fb_flip();
-JsVar *jswrap_fb_clear();
-int jswrap_fb_get(int x, int y);
-JsVar* jswrap_fb_set(int x, int y, int c);
-JsVar* jswrap_fb_fill(int x, int y, int w, int h);
-JsVar* jswrap_fb_set_color(int r, int g, int b);
 JsVar *jswrap_fb_prepare_blit(int x, int y);
 JsVar *jswrap_fb_blit(JsVar* buffer, int w, int h);
 
+int jswrap_fb_init();
 int jswrap_fb_create_rect(JsVar* opt);
-int jswrap_fb_update_rect(JsVar* opt);
-int jswrap_fb_render_rect();
+int jswrap_fb_update_rect(int id, JsVar* opt);
+int jswrap_fb_render();
+
+JsVar* jswrap_fb_color(int r, int g, int b);
